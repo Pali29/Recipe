74c8959aea55bb7cc16a8df3dd4977a6b00bf296
Ayush Paliwal <ayushpaliwal227@gmail.com>
2024-04-07 14:11:36 +0530

    adding the url to implement import of data from a website as a json file using the Retrofit framework in kotlin.

--- app/src/main/java/ind/pali/recipe/Apiservice.kt
+++ app/src/main/java/ind/pali/recipe/Apiservice.kt
@@ -1,2 +1,16 @@
 package ind.pali.recipe
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+
+private val retrofit = Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+        val recipeservice = retrofit.create(Apiservice::class.java)
+interface Apiservice{
+    @GET("categories.php")
+    suspend fun getcategories():Categoriesresponse
+}
